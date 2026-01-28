8621de728de54eefc0c9d0472a2db21efe5af086
Bhanutejadomala <bhanutejadomala@gmail.com>
2026-01-28 14:15:26 +0530

    message

---
+++ Backend/routes/pasteRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { createPaste, getPaste } = require("../controllers/pasteController");
+
+const router = express.Router();
+
+router.post("/", createPaste);
+router.get("/:id", getPaste);
+
+module.exports = router;
